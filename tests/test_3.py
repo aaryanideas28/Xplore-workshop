9ff3f84a41a8201a7267e7fde7b79cdc3be0e349
Once-1296 <awwab.wadekar@gmail.com>
2026-02-16 17:28:10 +0530

    added new tests

---
+++ tests/test_3.py
@@ -0,0 +1,7 @@
+def reverse(arr:list) -> list:
+    # TODO: Fix implementation
+    return arr
+
+def access_at_index(arr:list,idx:int)->object:
+    # TODO: Fix implementation
+    return arr[idx-1]
---
+++ tests/test_5.py
@@ -0,0 +1,5 @@
+def reverse_map(map:dict)->dict:
+    rmap = []
+    for key,value in map.items():
+        rmap[key]=value
+    return rmap
